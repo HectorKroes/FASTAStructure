14887e7a1e17cb301d47a7d854935d6e86e30dc2
Hector Fugihara Kroes <hector.kroes@outlook.com>
2020-06-12 14:34:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from results import *
+print('Program sync completed!')
+
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+from Bio import SeqIO
+print('Modules imported!')
+
+record = SeqIO.read("2WFU_A.fasta", format="fasta")
+print('Sequence read!')
+
+result_handle = NCBIWWW.qblast("blastp", "pdb", record.seq)
+print('BLAST done!')
+
+with open(tmpf + "blastp.xml", "w") as out_handle:
+	out_handle.write(result_handle.read())
+out_handle.close()
+print('XML file created!')
+
+readxml("blastp.xml")
